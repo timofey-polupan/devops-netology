80f7b319b4d678ca7358c36dd1ad44e878e42259
timofey-polupan <timofey.polupan@gmail.com>
2021-02-17 01:34:53 +0300

    git rebase 21

--- branching1/rebase.sh
+++ branching1/rebase.sh
@@ -3,7 +3,27 @@
 #display command line options
 
 count=1
-for param in "$*"; do
-echo "\$* Parameter #$count = $param"
-count=$(($count+1))
+for
+param
+in
+"
+$@
+"
+;
+do
+echo
+"
+Parameter:
+$param
+"
+count=
+$((
+$count
++
+1
+))
 done
+echo
+"
+=====
+"
